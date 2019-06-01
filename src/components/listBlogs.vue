cf4584664e2dacaa96190642097f3e0961d50342
dudedean <aima10.aima11@gmail.com>
2019-06-01 21:41:46 +0800

    -Lesson 38 mixin(reuse computed script into other components)

--- src/components/listBlogs.vue
+++ src/components/listBlogs.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="show-blogs">
-    <h1>All Blog Articles</h1>
+    <h1>List Blog Titles</h1>
     <input type="text" v-model="search" placeholder="search blogs"/>
     <div class="single-blog" v-for="blog in filteredBlogs" v-bind:key="blog.id">
         <h2 v-rainbow>{{blog.title | to-uppercase }}</h2>
-        <article>{{blog.body | snippet }}</article>
     </div>
 
   </div>
