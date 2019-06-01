de06fdd09bbfe7dddb0ab7a3dbf36d9788b53bb1
dudedean <aima10.aima11@gmail.com>
2019-06-01 13:55:50 +0800

    - Lesson 29, Input binding

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import addBlog from './components/addBlog.vue';
 
-export default {
-  name: 'app',
+export default{
   components: {
-    HelloWorld
-  }
+    'add-blog': addBlog
+  },
+  data() {
+    return {
+      
+    }
+  },
+  methods: {
+    
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  margin: 0;
+  font-family: 'Nunito';
 }
 </style>
