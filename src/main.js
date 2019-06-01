e58b5a4a61f37fc5c188985a2b83e0060e1655b4
dudedean <aima10.aima11@gmail.com>
2019-06-01 21:50:52 +0800

    -Lecture 39 Routing using vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+import Routes from './routes'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+});
 
 Vue.filter('snippet',function(value){
   return value.slice(0,100) + '...';
@@ -12,4 +19,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router: router
 }).$mount('#app')
